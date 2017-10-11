7d190d54793040f8d2f381b54a61859d2db9cf8c
Tyler Lawson <lawsont2@msu.edu>
2017-10-10 20:52:41 -0400

    proc 5

---
+++ Week_05/proj05.cpp
@@ -0,0 +1,63 @@
+// beaufort cipher, http://practicalcryptography.com/ciphers/classical-era/beaufort/
+
+#include<iostream>
+using std::cout; using std::cin; using std::endl;
+#include<string>
+using std::string;
+
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+
+int main (){
+  int case_number;
+  cin >> case_number;
+
+  switch (case_number){
+
+  case 1: {
+    int rotations;
+    cin >> rotations;
+    string s;
+    cin >> s;
+    cout << rotate_left(rotations, s) << endl;
+    break;
+  }
+    
+  case 2: {
+    char plain_letter;
+    char key_letter;
+    cin >> plain_letter >> key_letter;
+    cout << beaufort_letter(plain_letter, key_letter) << endl;
+    break;
+  }
+
+  case 3:{
+    char plain_letter;
+    char key_letter;
+    cin >> plain_letter >> key_letter;
+    cout << vigenere_letter(plain_letter, key_letter) << endl;
+    break;
+  }
+
+  case 4:{
+    string plain_text;
+    string key;
+    cin.ignore(100, '\n');
+    getline(cin, plain_text);
+    getline(cin, key);
+    cout << encode_beaufort(plain_text, key) << endl;
+    break;
+  }
+
+  case 5:{
+    string plain_text;
+    string key;
+    cin.ignore(100, '\n');
+    getline(cin, plain_text);
+    getline(cin, key);
+    cout << encode_vigenere(plain_text, key) << endl;
+    break;
+  }
+
+  } // of switch
+} // of main
